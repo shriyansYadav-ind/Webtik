70bcf002655093a69fc35ce47319d22b6d80f49f
shriyansYadav-ind <anayshriyans@gmail.com>
2025-11-15 15:31:19 +0530

    Create Comment model with Mongoose schema

---
+++ Comment.js
@@ -0,0 +1,12 @@
+// src/models/Comment.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
